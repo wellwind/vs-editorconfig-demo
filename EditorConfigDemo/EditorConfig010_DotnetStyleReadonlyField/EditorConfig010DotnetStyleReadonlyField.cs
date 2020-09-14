6642a3abc95aced9a2a829d761391d49d952d68f
黃升煌 Mike <mike@miniasp.com>
2020-09-14 15:11:45 +0800

    Demo dotnet_style_readonly_field

---
+++ EditorConfigDemo/EditorConfig010_DotnetStyleReadonlyField/EditorConfig010DotnetStyleReadonlyField.cs
@@ -0,0 +1,9 @@
+﻿namespace EditorConfigDemo.EditorConfig010_DotnetStyleReadonlyField
+{
+    class EditorConfig010DotnetStyleReadonlyField
+    {
+        // dotnet_style_readonly_field = true:suggestion
+        // 建議加上 readonly
+        private int _daysInYear = 365;
+    }
+}
