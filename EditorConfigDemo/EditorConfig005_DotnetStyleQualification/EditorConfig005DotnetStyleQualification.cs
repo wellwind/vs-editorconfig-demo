1a9d7fc80fde0b44750f8be55d915d91681eafd3
黃升煌 Mike <mike@miniasp.com>
2020-09-14 11:44:02 +0800

    Demo dotnet_style_qualification_*

---
+++ EditorConfigDemo/EditorConfig005_DotnetStyleQualification/EditorConfig005DotnetStyleQualification.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EditorConfigDemo.EditorConfig005_DotnetStyleQualification
+{
+    public class EditorConfig005DotnetStyleQualification
+    {
+        public string Name { get; set; }
+
+        private int capacity = 0;
+
+        public event EventHandler MyEvent;
+
+        public void Test()
+        {
+            // dotnet_style_qualification_for_field = false
+            this.capacity = 1;
+
+            // dotnet_style_qualification_for_property = false
+            this.Name = "Mike";
+
+            // dotnet_style_qualification_for_method = false
+            this.Test2();
+
+            // dotnet_style_qualification_for_event = false
+            this.MyEvent += EditorConfig004DotnetStyleQualification_MyEvent;
+        }
+
+        public void Test2()
+        {
+
+        }
+
+        private void EditorConfig004DotnetStyleQualification_MyEvent(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
