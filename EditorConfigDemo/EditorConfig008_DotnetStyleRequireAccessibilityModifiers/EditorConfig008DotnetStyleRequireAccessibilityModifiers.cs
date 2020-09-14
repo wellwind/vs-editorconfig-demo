a5e418698e8ecb3ea76e0765e380b0716de92421
黃升煌 Mike <mike@miniasp.com>
2020-09-14 15:24:09 +0800

    .editorconfig for dotnet_style_require_accessibility_modifiers

--- EditorConfigDemo/EditorConfig008_DotnetStyleRequireAccessibilityModifiers/EditorConfig008DotnetStyleRequireAccessibilityModifiers.cs
+++ EditorConfigDemo/EditorConfig008_DotnetStyleRequireAccessibilityModifiers/EditorConfig008DotnetStyleRequireAccessibilityModifiers.cs
@@ -1,5 +1,7 @@
 ﻿namespace EditorConfigDemo.EditorConfig008_DotnetStyleRequireAccessibilityModifiers
 {
+    // dotnet_style_require_accessibility_modifiers = for_non_interface_members
+    // 建議針對存取範圍加上修飾子 (private, public, internal, protected)
     class EditorConfig008DotnetStyleRequireAccessibilityModifiers
     {
         // dotnet_style_require_accessibility_modifiers = for_non_interface_members
