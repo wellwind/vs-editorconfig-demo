16e9a931a4d3fab9dbfec4c004a5ce31efbc5748
黃升煌 Mike <mike@miniasp.com>
2020-09-14 12:00:18 +0800

    Demo dotnet_style_predefined_type_for_*

---
+++ EditorConfigDemo/EditorConfig006_DotnetStylePredefinedType/EditorConfig006DotnetStylePredefinedType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EditorConfigDemo.EditorConfig006_DotnetStylePredefinedType
+{
+    // dotnet_style_predefined_type_for_* 系列設定在 format 時不會自動修正
+    public class EditorConfig006DotnetStylePredefinedType
+    {
+        // 建議使用內建的關鍵字 int
+        private Int32 _member;
+
+        public void Test()
+        {
+            // 建議使用內建的關鍵字 int
+            var local = Int32.MaxValue;
+        }
+    }
+}
