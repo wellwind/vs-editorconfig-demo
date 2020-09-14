aacfedc96ed566672e25e96b8067b8ce088cbb5a
黃升煌 Mike <mike@miniasp.com>
2020-09-14 12:26:14 +0800

    Demo dotnet_style_require_accessibility_modifiers

---
+++ EditorConfigDemo/EditorConfig008_DotnetStyleRequireAccessibilityModifiers/EditorConfig008DotnetStyleRequireAccessibilityModifiers.cs
@@ -0,0 +1,14 @@
+﻿namespace EditorConfigDemo.EditorConfig008_DotnetStyleRequireAccessibilityModifiers
+{
+    class EditorConfig008DotnetStyleRequireAccessibilityModifiers
+    {
+        // dotnet_style_require_accessibility_modifiers = for_non_interface_members
+        // 建議針對存取範圍加上修飾子 (private, public, internal, protected)
+        const string name = "Mike";
+
+        public string Test()
+        {
+            return name;
+        }
+    }
+}
