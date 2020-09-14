4a7af3bb7321df81c5a8819c2561b9c6a7c52e5a
黃升煌 Mike <mike@miniasp.com>
2020-09-14 15:25:44 +0800

    Demo dotnet_code_quality_unused_parameters

---
+++ EditorConfigDemo/EditorConfig011_DotnetCodeQualityUnusedParameters/All/EditorConfig011DotnetCodeQualityUnusedParametersAll.cs
@@ -0,0 +1,9 @@
+﻿namespace EditorConfigDemo.EditorConfig011_DotnetCodeQualityUnusedParameters.All
+{
+    class EditorConfig011DotnetCodeQualityUnusedParametersAll
+    {
+        // dotnet_code_quality_unused_parameters = all
+        // 沒用到的參數建議移除
+        public int GetNum(int arg1) { return 1; }
+    }
+}
---
+++ EditorConfigDemo/EditorConfig011_DotnetCodeQualityUnusedParameters/NonPublic/EditorConfig011DotnetCodeQualityUnusedParametersNonPublic.cs
@@ -0,0 +1,17 @@
+﻿namespace EditorConfigDemo.EditorConfig011_DotnetCodeQualityUnusedParameters.NonPublic
+{
+    class EditorConfig011DotnetCodeQualityUnusedParametersNonPublic
+    {
+        // 依照 editorconfig 設定這邊應該不會被標示問題才對
+        public int GetNum(int arg1) { return 1; }
+
+        // dotnet_code_quality_unused_parameters = non_public
+        // 只有非 public 的方法有沒用到的參數才會提示
+        int GetNum2(int arg2) { return 2; }
+
+        public void Test()
+        {
+            GetNum2(1);
+        }
+    }
+}
