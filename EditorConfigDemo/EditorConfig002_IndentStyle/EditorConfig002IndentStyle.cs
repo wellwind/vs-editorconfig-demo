ffb7de9acf4d9e8d46fc5fadfe10b4d9fe7a4eee
黃升煌 Mike <mike@miniasp.com>
2020-09-14 10:56:07 +0800

    Demo indend_style = tab, tab_width = 2

---
+++ EditorConfigDemo/EditorConfig002_IndentStyle/EditorConfig002IndentStyle.cs
@@ -0,0 +1,13 @@
+﻿namespace EditorConfigDemo.EditorConfig002_IndentStyle
+{
+    public class EditorConfig002IndentStyle
+    {
+        public string Test()
+        {
+            // 在 return 前按下 tab，該行會變成用 tab 顯示
+            // 打開 Tools -> Options -> Text Editor -> General -> View whitespace 會看的更清楚
+            // 雖然會用 tab 顯示，但在進行 format 時，依然會變成空白，應該是 bug
+            return "Mike";
+        }
+    }
+}
