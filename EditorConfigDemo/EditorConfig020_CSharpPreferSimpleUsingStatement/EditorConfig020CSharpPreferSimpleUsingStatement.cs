dae5ab7941e888d99dd00e86f6cb4347a2bd19a9
黃升煌 Mike <mike@miniasp.com>
2020-09-15 11:16:46 +0800

    Demo csharp_prefer_simple_using_statement

---
+++ EditorConfigDemo/EditorConfig020_CSharpPreferSimpleUsingStatement/EditorConfig020CSharpPreferSimpleUsingStatement.cs
@@ -0,0 +1,14 @@
+﻿namespace EditorConfigDemo.EditorConfig020_CSharpPreferSimpleUsingStatement
+{
+    class EditorConfig020CSharpPreferSimpleUsingStatement
+    {
+        public void Test()
+        {
+            // csharp_prefer_simple_using_statement = true
+            // 建議改為 using var fs = new System.IO.FileStream("test.txt", System.IO.FileMode.Open);
+            using (var fs = new System.IO.FileStream("test.txt", System.IO.FileMode.Open))
+            {
+            }
+        }
+    }
+}
