9e5467e864ec9e1d0ec687c99a62d90046d10524
黃升煌 Mike <mike@miniasp.com>
2020-09-15 10:36:57 +0800

    Demo csharp_preferred_modifier_order

---
+++ EditorConfigDemo/EditorConfig018_CSharpPreferredModifierOrder/EditorConfig018CSharpPreferredModifierOrder.cs
@@ -0,0 +1,9 @@
+﻿namespace EditorConfigDemo.EditorConfig018_CSharpPreferredModifierOrder
+{
+    class EditorConfig018CSharpPreferredModifierOrder
+    {
+        // csharp_preferred_modifier_order 設定 static 應該在 private 之後
+        // 因此建議改為 private static readonly int _daysInYear = 365;
+        static private readonly int _daysInYear = 365;
+    }
+}
