1f503e6b11d338456538310597db401e3f804709
黃升煌 Mike <mike@miniasp.com>
2020-09-15 10:33:08 +0800

    Demo csharp_prefer_static_local_function

---
+++ EditorConfigDemo/EditorConfig017_CSharpPreferStaticLocalFunction/EditorConfig017CSharpPreferStaticLocalFunction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EditorConfigDemo.EditorConfig017_CSharpPreferStaticLocalFunction
+{
+    class EditorConfig017CSharpPreferStaticLocalFunction
+    {
+        // csharp_prefer_static_local_function = true
+        // 建議改成 static void Test() { }
+        void Test()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
