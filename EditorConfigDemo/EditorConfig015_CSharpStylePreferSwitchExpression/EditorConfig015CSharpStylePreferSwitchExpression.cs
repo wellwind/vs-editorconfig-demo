1b78b96d1f12c7147c2313e92072e57588eee1e1
黃升煌 Mike <mike@miniasp.com>
2020-09-15 10:04:01 +0800

    Demo csharp_style_prefer_switch_expression

---
+++ EditorConfigDemo/EditorConfig015_CSharpStylePreferSwitchExpression/EditorConfig015CSharpStylePreferSwitchExpression.cs
@@ -0,0 +1,27 @@
+﻿namespace EditorConfigDemo.EditorConfig015_CSharpStylePreferSwitchExpression
+{
+    class EditorConfig015CSharpStylePreferSwitchExpression
+    {
+        public int Test()
+        {
+            var x = 1;
+            // csharp_style_prefer_switch_expression = true
+            // 建議改為
+            // return x switch
+            // {
+            //     1 => 1 * 1,
+            //     2 => 2 * 2,
+            //     _ => 0,
+            // };
+            switch (x)
+            {
+                case 1:
+                    return 1 * 1;
+                case 2:
+                    return 2 * 2;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
