1a74c761544e65ae781a177e3493a5958dfd2f98
黃升煌 Mike <mike@miniasp.com>
2020-09-15 11:11:14 +0800

    Demo csharp_prefer_braces

---
+++ EditorConfigDemo/EditorConfig019_CSharpPreferBraces/EditorConfig019CSharpPreferBraces.cs
@@ -0,0 +1,18 @@
+﻿namespace EditorConfigDemo.EditorConfig019_CSharpPreferBraces
+{
+    class EditorConfig019CSharpPreferBraces
+    {
+        public void Test()
+        {
+            var test = true;
+            // csharp_prefer_braces = true
+            // 建議寫法 if (test) { Display(); }
+            if (test) Display();
+        }
+
+        public void Display()
+        {
+
+        }
+    }
+}
