6f4163926230df5a9f6845e5acb0c4426c219366
黃升煌 Mike <mike@miniasp.com>
2020-09-15 11:48:29 +0800

    Demo csharp_using_directive_placementcsharp_using_directive_placement

---
+++ EditorConfigDemo/EditorConfig022_CsharpUsingDirectivePlacement/EditorConfig022CsharpUsingDirectivePlacement.cs
@@ -0,0 +1,15 @@
+﻿namespace EditorConfigDemo.EditorConfig022_CsharpUsingDirectivePlacement
+{
+    // csharp_using_directive_placementcsharp_using_directive_placement = outside_namespace
+    // using 應該在 namespace 外
+    // 這條設定 error 不會報錯
+    using System;
+
+    class EditorConfig022CsharpUsingDirectivePlacement
+    {
+        public void Test()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
