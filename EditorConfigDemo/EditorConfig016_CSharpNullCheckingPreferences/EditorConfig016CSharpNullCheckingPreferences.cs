daa700aca4fd188a5efbfcc5fda5aced75d33080
黃升煌 Mike <mike@miniasp.com>
2020-09-15 10:32:25 +0800

    Demo csharp_style_conditional_delegate_call

---
+++ EditorConfigDemo/EditorConfig016_CSharpNullCheckingPreferences/EditorConfig016CSharpNullCheckingPreferences.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EditorConfigDemo.EditorConfig016_CSharpNullCheckingPreferences
+{
+    class EditorConfig016CSharpNullCheckingPreferences
+    {
+        public Func<int, int> func = new Func<int, int>(x => x * x);
+
+        public void Test()
+        {
+            // csharp_style_conditional_delegate_call = true
+            // 建議改為 func?.Invoke(1);
+            if (func != null) { func(1); }
+        }
+    }
+}
