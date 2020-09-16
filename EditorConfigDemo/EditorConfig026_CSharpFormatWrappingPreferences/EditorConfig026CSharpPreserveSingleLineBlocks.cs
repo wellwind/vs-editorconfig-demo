c20571c1de3e177a43c48d8cc0e540761524076e
黃升煌 Mike <mike@miniasp.com>
2020-09-16 11:55:04 +0800

    Demo CSharp Format Wrapping preferences

---
+++ EditorConfigDemo/EditorConfig026_CSharpFormatWrappingPreferences/EditorConfig026CSharpPreserveSingleLineBlocks.cs
@@ -0,0 +1,13 @@
+﻿namespace EditorConfigDemo.EditorConfig026_CSharpFormatWrappingPreferences
+{
+    class EditorConfig026CSharpPreserveSingleLineBlocks
+    {
+        // csharp_preserve_single_line_blocks = true
+        // 這條不會自動 format，建議寫法為 public int MyProperty { get; set; }
+        // 但設成 false 會自動 format
+        public int MyProperty
+        {
+            get; set;
+        }
+    }
+}
---
+++ EditorConfigDemo/EditorConfig026_CSharpFormatWrappingPreferences/EditorConfigCSharpPreserveSingleLineStatements.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EditorConfigDemo.EditorConfig026_CSharpFormatWrappingPreferences
+{
+    class EditorConfigCSharpPreserveSingleLineStatements
+    {
+        public void TesT()
+        {
+            // csharp_preserve_single_line_statements = true
+            // 這條不會自動 format，建議寫法為 int i = 0; string name = "John";
+            // 但設成 false 會自動 format
+            int i = 0;
+            string name = "John";
+
+            Console.WriteLine(i);
+            Console.WriteLine(name);
+        }
+
+    }
+}
